af91b769d9894f920f2eb39d8f6c1232cea5551c
vroma <vromasov5@gmail.com>
2026-02-25 20:43:23 +0700

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,14 +75,6 @@ DATABASES = {
     }
 }
 
-# Оставляем SQLite для разработки как запасной вариант
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
